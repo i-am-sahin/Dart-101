b515d29894ca81292be009d17ed042f955915ae8
Sahin Islam <sahinislam878@.com>
2024-08-12 00:00:52 +0530

    Print Statement && Variables

---
+++ main.dart
@@ -0,0 +1,24 @@
+main() {
+  // print("Hello");
+
+  //Variables
+  var a = 10;
+  print(a);
+  a = 20;
+  print(a);
+  // a = "Hello"; //Error: A value of type 'String' can't be assigned to a variable of type 'int'.
+  print(a.runtimeType); // Gives datatype of A.
+
+  int age = 21;
+  print(age);
+  print(age.runtimeType);
+  String name = "Sahin";
+  print(name);
+  print(name.runtimeType);
+  double random = 121.0012;
+  print(random);
+  print(random.runtimeType);
+  bool isRaining = true;
+  print(isRaining);
+  print(isRaining.runtimeType);
+}
