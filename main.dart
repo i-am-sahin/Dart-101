322cfad630c023fb5c5ff1bff66500896edf301c
Sahin Islam <sahinislam878@.com>
2024-08-14 00:17:54 +0530

    Operators

--- main.dart
+++ main.dart
@@ -2,23 +2,46 @@ main() {
   // print("Hello");
 
   //Variables
-  var a = 10;
-  print(a);
-  a = 20;
-  print(a);
+  // var a = 10;
+  // print(a);
+  // a = 20;
+  // print(a);
   // a = "Hello"; //Error: A value of type 'String' can't be assigned to a variable of type 'int'.
-  print(a.runtimeType); // Gives datatype of A.
+  // print(a.runtimeType); // Gives datatype of A.
 
-  int age = 21;
-  print(age);
-  print(age.runtimeType);
-  String name = "Sahin";
-  print(name);
-  print(name.runtimeType);
-  double random = 121.0012;
-  print(random);
-  print(random.runtimeType);
-  bool isRaining = true;
-  print(isRaining);
-  print(isRaining.runtimeType);
+  // int age = 21;
+  // print(age);
+  // print(age.runtimeType);
+  // String name = "Sahin";
+  // print(name);
+  // print(name.runtimeType);
+  // double random = 121.0012;
+  // print(random);
+  // print(random.runtimeType);
+  // bool isRaining = true;
+  // print(isRaining);
+  // print(isRaining.runtimeType);
+
+  //Operators
+
+  //Arithmatic Operators : + , - , * , / , %
+
+  int num1 = 10;
+  int num2 = 20;
+  print(num1 + num2); //Add two Numbers
+  print(num1 - num2); //Substract two Numbers
+  print(num1 * num2); //Multiply two Numbers
+  print(num1 / num2); //Devide two Numbers
+  print(num1 % num2); // Gives reminder
+
+  //Prefix Operator : ++, --
+
+  int a = 100;
+  print(a++); //First print the number then Increment the number by 1
+  print(a);
+  print(++a); //First Increment the number by 1 then print the number
+
+  print(a--); //First print the number then Decrement the number by 1
+  print(a);
+  print(--a); //First Decrement the number by 1 then print the number
 }
